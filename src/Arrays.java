2d119c18adb6115f02ad28b07aafc38709f67878
rootGirl <gaostephanie@icloud.com>
2019-02-01 10:58:54 -0800

    updates

---
+++ src/Arrays.java
@@ -0,0 +1,51 @@
+public class Arrays {
+    //Input: [0,1,0,2,1,0,1,3,2,1,2,1] Output: 6
+    public int trapRain(int[] arr){
+        int count=0;
+        int max =0;
+        int maxIndex=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+                maxIndex=i;
+            }
+        }
+        for(int i=0;i<maxIndex;i++) {
+            if (arr[i] > 0) {
+                int wall = arr[i];
+                while (i < maxIndex) {
+                    i++;
+                    if (arr[i] < wall) {
+                        count += Math.min(wall, max) - arr[i];
+                    } else if (arr[i] > wall) {
+                        wall = arr[i];
+                    }
+                }
+            }
+        }
+        for(int i=arr.length-1;i>maxIndex;i--) {
+            if (arr[i] > 0) {
+                int wall = arr[i];
+                while (i > maxIndex) {
+                    i--;
+                    if (arr[i] < wall) {
+                        count += Math.min(wall, max) - arr[i];
+                    } else if (arr[i] > wall) {
+                        wall = arr[i];
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args){
+        Arrays array = new Arrays();
+        int[] arr = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(array.trapRain(arr));
+
+
+    }
+
+}
